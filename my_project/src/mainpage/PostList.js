2cf88f1de10d55a6f70c09bcc118a66e2bcd2e7a
SergioDark21 <sergio.depalma@fides.it>
2022-08-29 16:43:12 +0200

    myPost & FriendPost

--- my_project/src/mainpage/PostList.js
+++ my_project/src/mainpage/PostList.js
@@ -6,22 +6,34 @@ let i = 10;
 export default function PostList() {
 
     const [sendRequest, setSendRequest] = useState(false)
-    const [data, setData] = useState(null)
-    const [user, setUser] = useState(null)
+    const [data, setData] = useState([])
+    const [user, setUser] = useState([])
     const [error, setError] = useState(null)
+
+    const [myPost, setMyPost] = useState(true);
+    
+    function changeToMyPost(value){
+        setMyPost(value);
+    }
+
+    function myOwn(value){
+        if(value.userId == localStorage.getItem('name')){
+            return value;
+        }
+        else return false;
+    }
+
+    function myFriends(value){
+        if(value.userId != localStorage.getItem('name')){
+            return value;
+        }
+        else return false;
+    }
 
     function updateValue(value) {
         i = value * 10
         setSendRequest(true)
     }
-
-    // function idToUser(user){
-    //     for(let i=0;i<10;i++){
-    //         // console.log(data)
-    //         setData(data.name = user.userId)
-    //         console.log(data)
-    //     }
-    // }
     
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -50,7 +62,6 @@ export default function PostList() {
         })
         .then(user => {
             setUser(user);
-            // idToUser(user);
         })
         .catch(error => {
             console.error("Error fetching data: ", error);
@@ -62,25 +73,68 @@ export default function PostList() {
     
     }, [sendRequest])
 
+    for(let a=0;a<100;a++){
+                if(data[a]?.userId){
+                    for(let b=0;b<10;b++){
+                        if(user[0]?.id){
+                            if(data[a].userId === user[b].id){
+                                setData(data[a].userId = user[b].name)
+                            }
+                        }
+
+                    }
+                }
+                
+    }   
+
     return(
         <div className='post'>
-            <div className='postContainer'>
-                {data?.slice(i-10,i).map(data => <div className='postBox'>
+            <button onClick={() => changeToMyPost(true)}>MY POST</button>
+            <span> | </span>
+            <button onClick={() => changeToMyPost(false)}>FRIEND'S POST</button>
+            {myPost ? null :
+                <div className='friendPostContainer'>
+                    {Array.from(data).filter(myFriends).slice(i-10,i).map(data => <div className='postBox'>
+                        <div className='authorPost'>
+                            <span>Posted by: </span>
+                            <span>{data.userId}</span>
+                            </div>
+                        <div className='titlePost'>{data.title.toUpperCase()}</div>
+                        <hr style={{fontWeight:'700'}}/>
+                        <div className='bodyPost'>{data.body}</div>
+                        </div>)}
+                </div>
+            }
+            {!myPost ? null : 
+                <div className='myPostContainer'>
+                {Array.from(data).filter(myOwn).map(data =>  <div className='postBox'>
                     <div className='authorPost'>
-                        <span>Posted by:</span>
+                        <span>Posted by: </span>
                         <span>{data.userId}</span>
                         </div>
-                    <div className='titlePost'>{data.title}</div>
+                    <div className='titlePost'>{data.title.toUpperCase()}</div>
                     <hr style={{fontWeight:'700'}}/>
                     <div className='bodyPost'>{data.body}</div>
-                    </div>)}
-            </div>
-            <div className="buttonBox">
-                {[...Array(10)].map((el, i) => (
+                    </div>
+                )}
+                </div>
+            }
+            {!myPost ? null :
+                <div className="buttonBox">
+                {[...Array(1)].map((el, i) => (
                     <button className='button' onClick={() => updateValue(i + 1)}>{i + 1}</button>
                 ))
                 }
             </div>
+            }
+            {myPost ? null :
+                <div className="buttonBox">
+                {[...Array(9)].map((el, i) => (
+                    <button className='button' onClick={() => updateValue(i + 1)}>{i + 1}</button>
+                ))
+                }
+            </div>
+            }
         </div>
         
     )
